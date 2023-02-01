3a17ab9007cc50178b9e314b4e6ae52b8cd31f7d
vallieres13 <mail@zachery.io>
2023-02-01 23:18:11 +0100

    Cleanup code

--- src/pages/Stories.tsx
+++ src/pages/Stories.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import gsap from 'gsap';
 import { CustomEase } from 'gsap/CustomEase';
 import { CSSPlugin } from 'gsap/CSSPlugin';
